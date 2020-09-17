1c9164eb0557127877931dd666490b35458c524a
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-17 22:51:31 +0530

    util.js to contain common utility functions
     implemented a function `setup` which can extract params from the request object

---
+++ util.js
@@ -0,0 +1,23 @@
+function hasprop(o,p) {
+ if(!o) return false;
+ if(o[p]) return true;
+ if(o.hasOwnProperty) return o.hasOwnProperty(p);
+ return Object.hasOwnProperty.call(o,p);
+}
+
+
+module.exports= {
+ params: {
+  setup: function(req,res,cb) {
+   if(!req.USERDATA) req.USERDATA = {};
+   req.USERDATA.P = function(n) {
+    if(hasprop(req.body,n)) return req.body[n];
+    if(hasprop(req.query,n)) return req.query[n];
+    if(hasprop(req.params,n)) return req.params[n];
+    if(hasprop(req.cookies,n)) return req.cookies[n];
+    return undefined;
+   };
+   cb();
+  }
+ }
+};
