5432385d0efb2c67cb053d54d8d14b1851ecf852
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-17 22:30:41 +0530

    allowing cross origin requests

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const HTTP = require('http');
 const EXPRESS = require('express');
 const app = EXPRESS();
 
+app.use(function(req,res,cb) {
+ res.set('Access-Control-Allow-Origin','*');
+ cb();
+});
+
 const server= HTTP.createServer(app);
 server.listen(3000,function(){
  console.log(`HTTP server listening on the port ${3000}`);
