50bf076a404929152916540ff48bd3c87334acec
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-17 22:57:54 +0530

    serving simple html page from node application

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const HTTP = require('http');
 const BODYPARSER = require('body-parser');
+const PATH = require('path');
 const EXPRESS = require('express');
 const app = EXPRESS();
 const U=require('./util');
 
+const htdocsdir = PATH.join(__dirname,'htdocs');
 app.use(function(req,res,cb) {
  res.set('Access-Control-Allow-Origin','*');
  cb();
@@ -11,6 +13,8 @@ app.use(function(req,res,cb) {
 
 app.use(BODYPARSER.urlencoded({extended:true})).use(BODYPARSER.json());
 app.use(U.params.setup);
+
+app.use(EXPRESS.static(htdocsdir));
 
 const server= HTTP.createServer(app);
 server.listen(3000,function(){
