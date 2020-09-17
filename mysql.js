9a30cfa6c8f3fa2ec22a4585962865c9d4b9642c
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-17 23:34:08 +0530

    conn function to get mysql connection

--- mysql.js
+++ mysql.js
@@ -9,4 +9,35 @@ module.exports = function(o) {
    return console.error({connname:c},'No mysql connection %s defined',c),null;
   return pools[c]=MYSQL.createPool(o.config[c]);
  }
+
+ function conn(c,cb) {
+  var P = pool(c);
+  if(!P) return cb(new Error('Failed to create mysql pool for '+c));
+  P.getConnection(function(e,C) {
+   if(e) return cb(e,C);
+   C.config.queryFormat = function(q,v) {
+    if(!v) return q;
+    return q.replace(/:(\w+)/g, function(t,k) {
+     return v.hasOwnProperty(k) ? ((v[k]===null)?'NULL':this.escape(v[k])) : t;
+    }.bind(this));
+   };
+   C.Q = function(q,p,cb) {
+    console.trace({query:q,args:p},'MySQL: %s',q);
+    const t0=new Date();
+    this.query(q,p,function(e,r) {
+     const t1=new Date();
+     const dt = t1-t0;
+     console.trace("MySQL query '%s' took %dms",q,dt);
+     cb(e,r);
+    });
+   };
+   cb(null,C);
+  });
+ }
+
+ return {
+  C: function(n,cb) {
+   return conn(n,cb);
+  }
+ };
 };
